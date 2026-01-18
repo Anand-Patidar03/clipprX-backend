5b2a1f22466bc99aef775503716e3a33d04fbb2c
anand patidar <anandpatidar2107@gmail.com>
2026-01-18 22:35:23 +0530

    debug app.js

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: [process.env.CORS_ORIGIN],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
